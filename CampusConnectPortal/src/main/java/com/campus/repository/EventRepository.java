c58142670359a540edc6cedbe19d5967e99d5dd4
Rudray Pandya <pandyarudray27@outlook.com>
2026-05-06 13:48:40 +0530

    Delete CampusConnectPortal directory

--- CampusConnectPortal/src/main/java/com/campus/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.campus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.campus.model.Event;
-
-public interface EventRepository extends JpaRepository<Event, Integer> {
-
-}
--- CampusConnectPortal/src/main/java/com/campus/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.campus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.campus.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-    Student findByEmailAndPassword(String email, String password);
-}
